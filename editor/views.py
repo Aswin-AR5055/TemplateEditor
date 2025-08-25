da900b29f61950f6e4d8ceae7e8302c4a66a5add
Aswin-AR5055 <aswinraj2003@gmail.com>
2025-08-25 22:24:46 +0530

    improved styles

--- editor/views.py
+++ editor/views.py
@@ -19,14 +19,12 @@ def template_editor(request):
             filename = fs.save(image.name, image)
             context['image_url'] = fs.url(filename)
 
-        # Background image (check uploaded, else fallback)
         if request.FILES.get('background_image'):
             background_image = request.FILES['background_image']
             fs = FileSystemStorage()
             filename = fs.save(background_image.name, background_image)
             context['background_image_url'] = fs.url(filename)
         else:
-            # default background (from static folder)
             context['background_image_url'] = '/static/editor/image/55123.jpg'
 
         return render(request, 'editor/template_preview.html', context)
